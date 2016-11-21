0c74910297a5eec6bec8688903ee52a650097a8b
mrobeunitn <marco.roberti@studenti.unitn.it>
2016-11-21 13:30:08 +0100

    Costruzione oggetto Impiegato

---
+++ data.js
@@ -0,0 +1,13 @@
+var employee = [];
+
+//funzione che utilizzo per mappare i dati in un oggetto di tipo operaio
+function mapEmployees(id,nome,cognome,livello,salario){
+    var impiegato =(
+        "id" : id;
+        "name" : nome;
+        "surname" : cognome;
+        "level" : livello;
+        "salary": salario;
+    )
+    employee.append(impiegato);
+}
